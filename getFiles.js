9699641c55dc8fbae5ca3ac273b556cbd068f8b0
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-10-16 23:33:50 +0100

    split module down

--- getFiles.js
+++ getFiles.js
@@ -1,6 +1,7 @@
 const Client = require('ftp')
 const fs = require('fs')
 const initDirectory = require('./initDirectory')
+const ftpGetAndSave = require('./ftpGetAndSave');
 
 function getFiles(instanceOptions) {
   return new Promise((resolve, reject) => {
@@ -51,36 +52,6 @@ function getFiles(instanceOptions) {
         })
       })
     }).catch((err) => reject(err))
-  })
-}
-
-// gets a file and saves it to a local directory/file
-function ftpGetAndSave(client, path, filename) {
-  return new Promise((resolve, reject) => {
-    // get file from ftp server
-    client.get(filename, (err, stream) => {
-      if (err) return reject(err)
-
-      // HACK: used to not resolve a promise that has close due to error
-      let hasError = false
-
-      // write file to path/name
-      stream.pipe(fs.createWriteStream(path + '/' + filename).on('error', (err) => {
-        hasError = true
-        return reject(err)
-      }))
-
-      // data has been written/no data left
-      stream.once('close', () => {
-        // if error has happened do nothing - part of hack above
-        if (!hasError) {
-          console.log('file downloaded: ', filename)
-          resolve()
-        }
-      })
-
-      stream.on('error', (err) =>  reject(err) )
-    })
   })
 }
 
