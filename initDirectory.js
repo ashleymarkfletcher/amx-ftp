0737273bf8ca250f57cf7360f83beca8f5dd7e2d
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-10-27 00:44:36 +0100

    try again

--- initDirectory.js
+++ initDirectory.js
@@ -4,7 +4,7 @@ const makeDirectory = require('./makeDirectory');
 
 // create directory if it doesn't exist
 function initDirectory (path) {
-  return new Promise( async function(resolve, reject) {
+  return new Promise( async (resolve, reject) => {
 
     const exists = await pathExists(path)
 
