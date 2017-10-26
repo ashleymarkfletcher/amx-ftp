956240ec723c4053ae0ead611dbd1c6d2ca5f29f
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-10-27 00:50:25 +0100

    formatting

--- initDirectory.js
+++ initDirectory.js
@@ -4,15 +4,11 @@ const makeDirectory = require('./makeDirectory');
 
 // create directory if it doesn't exist
 async function initDirectory (path) {
+  const exists = await pathExists(path)
 
+  if (exists) return Promise.resolve()
 
-    const exists = await pathExists(path)
-
-    if (exists) return Promise.resolve()
-
-    return makeDirectory(path)
-
+  return makeDirectory(path)
 }
-
 
 module.exports = initDirectory
