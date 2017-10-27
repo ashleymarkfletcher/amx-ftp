0850ff6fa2ca3313b2df07b94f00aa3c6766c899
ashley.fletcher <ashley.fletcher@soloworks.co.uk>
2017-10-27 15:34:53 +0100

    removed testing code and split function

--- ftpGetAndSave.js
+++ ftpGetAndSave.js
@@ -7,28 +7,38 @@ function ftpGetAndSave(client, path, filename) {
     client.get(filename, (err, stream) => {
       if (err) return reject(err)
 
-      // HACK: used to not resolve a promise that has close due to error
-      let hasError = false
-
-      // write file to path/name
-      stream.pipe(fs.createWriteStream(path + '/' + filename)
-      .on('error', (err) => {
-        hasError = true
-        return reject(err)
-      }))
-
-      // data has been written/no data left
-      stream.once('close', () => {
-        // if error has happened do nothing - part of hack above
-        if (!hasError) {
-          console.log('file downloaded: ', filename)
-          resolve()
-        }
-      })
-
-      stream.on('error', (err) =>  reject(err) )
+      writeFileFromStream(stream, path, filename)
+      .then(() => resolve())
+      .catch((err) => reject(err))
     })
   })
+}
+
+function writeFileFromStream(stream, path, filename) {
+  return new Promise(function(resolve, reject) {
+
+
+  // HACK: used to not resolve a promise that has close due to error
+  let hasError = false
+
+  // write file to path/name
+  stream.pipe(fs.createWriteStream(path + '/' + filename)
+  .on('error', (err) => {
+    hasError = true
+    return reject(err)
+  }))
+
+  // data has been written/no data left
+  stream.once('close', () => {
+    // if error has happened do nothing - part of hack above
+    if (!hasError) {
+      console.log('file downloaded: ', filename)
+      resolve()
+    }
+  })
+
+  stream.on('error', (err) =>  reject(err) )
+    });
 }
 
 module.exports = ftpGetAndSave
